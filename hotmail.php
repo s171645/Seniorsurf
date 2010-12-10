f8bcdedc6dd62655becb08eba58fd6f805ffe554
Vijitharan <Vijitharan91@gmail.com>
2010-12-10 22:34:54 +0100

    files from 10.12.2010

--- hotmail.php
+++ hotmail.php
@@ -13,9 +13,37 @@ include "topp.html";
   <param name="allowscriptaccess" value="always">
   <embed src="http://www.youtube.com/v/iBzNwp1RJhI?fs=1&amp;hl=nb_NO&amp;rel=0" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="560" height="340">
 </object>
-<p>Gå inn på www.hotmail.com  Trykk registrer og fyll inn hotmail adresse og klikk kontroller tilgjenglighet. Da får du vite om du kan ha det navnet eller ikke.  Også fyller du ut resten og klikker Jeg godtar. Nå har du laget en epostkonto.</p>
 
-<p>Link til <a href="http://www.hotmail.com">Hotmail </a> </p>
+<ol>
+ <li>
+ <p> Gå inn på <a href="http://www.hotmail.com">www.hotmail.com</a> </p>
+   </li>
+    <li> 
+	<p> Trykk på "Sign up" nede til venstere.</p>
+	  </li>
+	   <li>
+	     <p>Du skal nå velge navnet på e-posten din, det er greit å ha denne enklest mulig så det er lett å huske og fortelle til andre.</p>
+		  <p>Merk at vært e-post navn må være unikt, vis du trykker på "Check availabillity" sjekker den om e-post navnet allerede brukes.</p>
+		  </li>
+		   <li>
+		   <p>Lag et vanskelig passord som er lett å huske, anbefalt 8 karakterer med forskjellige teng og ikke personlige passord, (som navnet på hunden din f.eks) disse blir lette å gjette. </p>
+		    <p>(case sensitive betyr at den gjør forskjell på liten og stor bokstav). </p>
+	        </li>
+			 <li>
+			  <p>Fyll ut resten av informasjonen det bes om.</p>
+			   </li>
+		      <li>
+			   <p>I den siste boksen skal du skrive inn de to ordene du ser rett over boksen, hvis du har problemer med å se hva som står der </p>
+			    <p>trykker du på "New" får å få en annen eller "Audio" for å høre hva du skal skrive inn.</p>
+				</li>
+				 <li>
+				  <p> Trykk på "I accept", og du skal nå ha en hotmail e-post konto.
+				   </li>
+			        <li>
+					<p>Gå inn på <a href="http://www.hotmail.com">www.hotmail.com</a>, der det står "windows live ID" skriver du inn navnet på e-post kontoen din. </p>
+				 	<p> Skriv inn passordet ditt under og trykk på "Sign in" </p>
+					 </li>
+					</ol>
 </div>
 
 <?php
--- video.php
+++ video.php
@@ -6,15 +6,25 @@ include "topp.html";
 												<!-- Brødtekst -->
 			 <div id="main"> 
 			 <h2> Video leksjoner </h2>
-<p>Her ser du hvordan du skal gjøre det større:</p>
+<p>Her ser du hvilken knapp du må trykke på for å få fullskjerm når du spiller av en video: </p>
 <img src="youtube.jpg" alt="** Bilde som viser hvordan man forstørrer">
 
 <p>Videoleksjoner:</p>
-<p><a href="google.php" > Videoklipp om hvordan man bruker Google </a> </p>
-<p><a href="gulesider.php" > Videoklipp om hvordan man bruker Gulesider</a> </p>
-<p><a href="hotmail.php" > Videoklipp om hvordan man oppretter Hotmail konto </a> </p>
-<p><a href="facebook.php" > Videoklipp om hvordan man oppretter Facebook konto </a> </p>
-			  </div>
+<ul>
+   <li>
+	<p><a href="google.php" > Videoklipp om hvordan man bruker Google </a> </p>
+     </li>
+   <li>
+	<p><a href="gulesider.php" > Videoklipp om hvordan man bruker Gulesider</a> </p>
+	 </li>
+	<li>
+	 <p><a href="hotmail.php" > Videoklipp om hvordan man oppretter Hotmail konto </a> </p>
+	  </li>
+	 <li>
+	  <p><a href="facebook.php" > Videoklipp om hvordan man oppretter Facebook konto </a> </p>
+	  </li>
+ </ul>	  
+			 </div>
 			  
 <?php
 include "bunn.html";
