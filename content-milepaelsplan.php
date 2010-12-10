e05094642fcfa57bf632abae1e474e491fa64faf
Vijitharan <Vijitharan91@gmail.com>
2010-12-10 22:36:14 +0100

    milepaelsplan og ansvarskart

---
+++ content-milepaelsplan.php
@@ -0,0 +1,45 @@
+
+<h2><strong>Milepælsplan</strong></h2>
+		
+		<p>
+			<strong>1.Levering</strong>
+		</p>
+		
+<table class="default">
+
+	<tr><th>Status </th><th>Aktivitet </th><th>Beskrivelse </th><th>Ansvarlig </th><th>Ferdigstillingsdato </th>
+	<tr><td>OK </td><td>Bestemme tema </td><td> Hva vi skal jobbe med </td><td>Alle </td><td>04.10.2010 </td></tr>
+	<tr><td>OK </td><td>Fordeling av oppgaven </td><td>Fordele arbeidet </td><td>Alle </td><td>04-05.10.2010 </td></tr>
+	<tr><td>OK </td><td>Lagringsenhet </td><td>Felles lagringsenhet </td><td>Alle </td><td> 04.10.2010 </td></tr>
+	<tr><td>OK </td><td>Prosjektbeskrivelse </td><td>"Den røde tråden" i prosjektet</td><td>Kristina </td><td>05.10.2010 </td></tr>
+	<tr><td>OK </td><td>Samarbeidskontrakt </td><td>Dokumenterer avtalene våre </td><td>Viji </td><td>05.10.2010 </td></tr>
+	<tr><td>OK </td><td>Risikoplan </td><td>Hvilke problemer vi kan møte og hvordan vi hindrer </td><td>Kristoffer </td><td>12.10.2010 </td></tr>
+	<tr><td>OK </td><td>Milepælsplan </td><td>Lage milepælsplan </td><td>Viji </td><td> 04.10.2010 </td></tr>
+	<tr><td>OK </td><td>Aktivitetsplan </td><td>Skrive aktivitetsplan </td><td>Kristoffer </td><td>06.10.2010 </td></tr>
+	<tr><td>OK </td><td>Første utkast </td><td>Lage førsteutkast av siden </td><td>Kristina </td><td>06.10.2010 </td></tr>
+	<tr><td>OK </td><td>Prosjektdagbok 1</td><td>Notater fra møtene </td><td>Stafeni </td><td>13.10.2010 </td></tr>
+	<tr><td>OK </td><td>Innføring </td><td>Innføring av dokumentene til hjemmesiden </td><td>Kristina </td><td>10.10.2010 </td></tr>  
+	<tr><td>OK </td><td>Leveranse 1 </td><td>Laste opp dokumenter </td><td>Gruppen </td><td>13.10.2010 </td></tr> 
+	
+	<tr><td>OK </td><td>Domene</td><td>Skaffe sted </td><td>Viji </td><td>25.10.2010 </td></tr>	 
+	<tr><td>OK </td><td>Midtveisrapport</td><td>Skrive midtveisrapport </td><td>Kristina </td><td>29.10.2010 </td></tr>	 	
+	<tr><td>OK </td><td>Milepælsplan </td><td>Oppdatere milepælsplan </td><td>Viji </td><td> 01.11.2010 </td></tr>
+	<tr><td>OK </td><td>Design</td><td>Designe siden </td><td>Kristoffer </td><td>03.11.2010 </td></tr>	
+	<tr><td>OK </td><td>Aktivitetsplan </td><td>oppdattere aktivitetsplan </td><td>Kristoffer </td><td>04.11.2010 </td></tr> 	
+	<tr><td>OK </td><td>Videoer</td><td>Lage videoer til siden </td><td>Viji </td><td>03.11.2010 </td></tr>
+	<tr><td>OK </td><td>Skisse </td><td>Skisse til sluttrapport</td><td>Kristoffer </td><td>04.11.2010 </td></tr>
+	<tr><td>OK </td><td>Prosjektdagbok 2</td><td>Notater fra møtene </td><td>Stafeni </td><td>05.11.2010 </td></tr>
+	<tr><td>OK </td><td>Oppdatere alt </td><td>Oppdatere styringsdokumentsiden </td><td>Kristina </td><td>05.11.2010 </td></tr>
+	<tr><td>OK </td><td>Leveranse 2 </td><td>Laste opp dokumenter </td><td>Gruppen </td><td>08.11.2010 </td></tr>   
+	
+	<tr><td>OK </td><td>Milepælsplan </td><td>Oppdatere milepælsplan </td><td>Viji </td><td> 10.12.2010 </td></tr>
+	<tr><td>IKKE OK </td><td>Aktivitetsplan </td><td>oppdattere aktivitetsplan </td><td>Kristoffer </td><td>10.12.2010 </td></tr>
+	<tr><td>OK </td><td>Ansvarskartet </td><td>oppdattere ansvarskart </td><td>Viji </td><td>13.12.2010 </td></tr> 
+	<tr><td>IKKE OK </td><td>Ferdigstilling av siden </td><td>Siden skal være ferdig </td><td>Gruppen </td><td>14.12.2010 </td></tr>     
+	<tr><td>IKKE OK </td><td>Prosjektdagbok 3</td><td>Notater fra møtene </td><td>Stafeni </td><td>16.11.2010 </td></tr>    
+	<tr><td>IKKE OK </td><td>Teste </td><td>Fulstendig testing </td><td>Gruppen </td><td>16.11.2010 </td></tr>  
+	<tr><td>IKKE OK </td><td>Sluttrapporten </td><td>Få ferdig sluttrapporten </td><td>Gruppen </td><td>16.12.2010 </td></tr>  
+	<tr><td>IKKE OK </td><td>Leveranse 3 </td><td>Laste opp dokumenter </td><td>Gruppen </td><td>17.12.2010 </td></tr>    	
+</table>
+
+		
