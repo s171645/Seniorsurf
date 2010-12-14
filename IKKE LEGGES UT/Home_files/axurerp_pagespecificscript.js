53bf47a9f092a196ef0e88e05bb0e471b5ea0d8d
Vijitharan <Vijitharan91@gmail.com>
2010-12-14 23:36:39 +0100

    AXURE

---
+++ IKKE LEGGES UT/Home_files/axurerp_pagespecificscript.js
@@ -0,0 +1,121 @@
+
+var PageName = 'Home';
+var PageId = 'p10f1dc84a4ad484da0d8ab3a36e65234'
+var PageUrl = 'Home.html'
+document.title = 'Home';
+
+if (top.location != self.location)
+{
+	if (parent.HandleMainFrameChanged) {
+		parent.HandleMainFrameChanged();
+	}
+}
+
+var $OnLoadVariable = '';
+
+var $CSUM;
+
+var hasQuery = false;
+var query = window.location.hash.substring(1);
+if (query.length > 0) hasQuery = true;
+var vars = query.split("&");
+for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0].length > 0) eval("$" + pair[0] + " = decodeURIComponent(pair[1]);");
+} 
+
+if (hasQuery && $CSUM != 1) {
+alert('Prototype Warning: The variable values were too long to pass to this page.\nIf you are using IE, using Firefox will support more data.');
+}
+
+function GetQuerystring() {
+    return '#OnLoadVariable=' + encodeURIComponent($OnLoadVariable) + '&CSUM=1';
+}
+
+function PopulateVariables(value) {
+  value = value.replace(/\[\[OnLoadVariable\]\]/g, $OnLoadVariable);
+  value = value.replace(/\[\[PageName\]\]/g, PageName);
+  return value;
+}
+
+function OnLoad(e) {
+
+}
+
+var u20 = document.getElementById('u20');
+gv_vAlignTable['u20'] = 'top';
+var u36 = document.getElementById('u36');
+
+var u31 = document.getElementById('u31');
+
+var u11 = document.getElementById('u11');
+gv_vAlignTable['u11'] = 'center';
+var u27 = document.getElementById('u27');
+
+var u6 = document.getElementById('u6');
+
+var u4 = document.getElementById('u4');
+
+var u2 = document.getElementById('u2');
+
+var u10 = document.getElementById('u10');
+
+var u0 = document.getElementById('u0');
+
+var u26 = document.getElementById('u26');
+gv_vAlignTable['u26'] = 'center';
+var u35 = document.getElementById('u35');
+
+var u29 = document.getElementById('u29');
+
+var u8 = document.getElementById('u8');
+
+var u34 = document.getElementById('u34');
+gv_vAlignTable['u34'] = 'center';
+var u14 = document.getElementById('u14');
+gv_vAlignTable['u14'] = 'center';
+var u28 = document.getElementById('u28');
+gv_vAlignTable['u28'] = 'center';
+var u33 = document.getElementById('u33');
+
+var u22 = document.getElementById('u22');
+gv_vAlignTable['u22'] = 'top';
+var u13 = document.getElementById('u13');
+
+var u12 = document.getElementById('u12');
+gv_vAlignTable['u12'] = 'top';
+var u21 = document.getElementById('u21');
+gv_vAlignTable['u21'] = 'top';
+var u37 = document.getElementById('u37');
+gv_vAlignTable['u37'] = 'center';
+var u7 = document.getElementById('u7');
+gv_vAlignTable['u7'] = 'center';
+var u17 = document.getElementById('u17');
+gv_vAlignTable['u17'] = 'center';
+var u5 = document.getElementById('u5');
+gv_vAlignTable['u5'] = 'center';
+var u15 = document.getElementById('u15');
+
+var u3 = document.getElementById('u3');
+gv_vAlignTable['u3'] = 'center';
+var u1 = document.getElementById('u1');
+gv_vAlignTable['u1'] = 'center';
+var u25 = document.getElementById('u25');
+
+var u16 = document.getElementById('u16');
+
+var u19 = document.getElementById('u19');
+gv_vAlignTable['u19'] = 'top';
+var u9 = document.getElementById('u9');
+gv_vAlignTable['u9'] = 'center';
+var u30 = document.getElementById('u30');
+gv_vAlignTable['u30'] = 'center';
+var u24 = document.getElementById('u24');
+gv_vAlignTable['u24'] = 'center';
+var u18 = document.getElementById('u18');
+gv_vAlignTable['u18'] = 'top';
+var u32 = document.getElementById('u32');
+gv_vAlignTable['u32'] = 'center';
+var u23 = document.getElementById('u23');
+
+if (window.OnLoad) OnLoad();
