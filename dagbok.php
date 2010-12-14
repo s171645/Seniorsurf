1199fdde3b5c8846a5d90d4c915715716726b553
Vijitharan <Vijitharan91@gmail.com>
2010-12-14 23:35:28 +0100

    Leveranse 2

---
+++ dagbok.php
@@ -0,0 +1,17 @@
+
+<h2><strong>Fortsettelse på dagbok </strong></h2>
+		
+
+			
+22.10.10 <br>
+Stafeni var bortreist i dag og tre fra gruppen møtte opp og snakket om leveranse 2 på datatorget. Alle jobbet hver for seg. Kristina delte opp oppgaven i en strukturert måte slik at det var enklere å lese oppgaven. Kristoffer og Viji jobbet med selve nettsiden. Kristoffer bestilte gruppe rom til 28.10.10 og 04.11.10 slik at vi kunne jobbe aktivt uten forstyrelser. <br></br>
+
+
+27.10.10<br>
+Alle fra gruppen var til stede rundt klokken 09.00 på datatorget. Vi snakket litt om hvor langt vi hadde kommet med hver vår oppgave. Etter det tegnet vi hver vår skisse til nettsiden, og alle kom med nesten samme forslag. Til slutt sammenlignet vi tegningene og tok med litt av hver enkelts ide. Deretter forsatte Kristoffer og Viji med å skissere ideen vår på programmet Axcer, mens Kristina og Stafeni begynte med evaluering av prosjektarbeidet. Underveis i arbeidet diskuterte vi vår innsats og samarbeid i prosjektet, som vi skrev ned i evalueringen.  Etter en stund ble vi både ferdig med skissen og evalueringen. I dag jobbet vi ganske effektivt og gjorde en kjempe bra innsats. Og vi håper på at dette forsette videre.<br></br>
+
+
+04.11.10<br>
+Idag var det bare tre som var tilstede men vi hadde kontakt med sistemann. Vi fikk en oversikt over hvor mye som er igjen og begynte med å legge ting inn på selve siden. Vi har fått opp noen videoklipper og en del tekst.<br></br>
+ 
+
