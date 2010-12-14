f79ac2178e98f10c57494b608acea18c76a1da20
Vijitharan <Vijitharan91@gmail.com>
2010-12-14 23:32:29 +0100

    leveranse 1 files

--- linker.php
+++ linker.php
@@ -10,35 +10,108 @@ include "topp.html";
 												<!-- Brødtekst -->
 			 <div id="main"> 
 			 <h2> Lenker </h2>
+<strong>Nyheter:</strong>
+<div id="katogori">
+<div class="kat">
+<a href="http://www.aftenposten.no">Aftenposten </a>  
+<a href="http://www.vg.no">VG </a> 
+<a href="http://www.nrk.no">NRK </a> 
+<a href="http://www.dagbladet.no">Dagbladet </a> 
+<a href="http://www.dn.no">DagensNæringsliv </a>
+<a href="http://www.abcnyheter.no/">ABCnyheter </a>
+<a href="http://www.tv2nyhetene.no/">TV2 </a>
+<hr/>
+<strong>Utenriks:</strong>
+<a href="http://www.cnn.com/">CNN</a>
+<a href="http://www.bbc.co.uk/">BBC</a>
+<a href="http://www.nytimes.com/">NewYorkTimes</a>
+
+</div>
+</div>
 
 
-Epost:<br>
-<p><a href="http://www.hotmail.com">Hotmail </a> </p>
-<p><a href="http://www.gmail.com">Gmail </a> </p>
 
-Nyheter:<br>
-<p><a href="http://www.aftenposten.no">Aftenposten </a>  </p>
-<p><a href="http://www.vg.no">VG </a> <br>
-<p><a href="http://www.nrk.no">NRK </a> <br>
-<p><a href="http://www.dagbladet.no">Dagbladet </a> </p>
+<strong>Epost:</strong>
+<div id="katogori">
+<div class="kat">
+<strong>Web baserte:</strong>
+<a href="http://www.hotmail.com">Hotmail </a>
+<a href="https://login.yahoo.com/config/login_verify2?&.src=ym">Yahoo</a>
+<a href="https://my.screenname.aol.com">AOL</a>
+<a href="http://www.gmail.com">Gmail </a>
+<a href="http://signup.lus.com:81/index.php/component/user/register">Lus</a>
+<a href="http://www.operamail.com/scripts/common/index.main?signin=1&lang=us">Opera</a>
+<hr/>
+<strong>Epost programvare:</strong>
 
-Data: <br>
-<p><a href="http://www.dinside.no">Dinside </a> </p>
-<p><a href="http://www.digi.no">Digi </a> </p>
-<p><a href="http://www.itavisen.no">Itavisen </a> </p>
+<a href="http://www.mozillamessaging.com/en-US/thunderbird/">Thunderbird </a>
+<a href="http://www.ritlabs.com/en/products/thebat/">Thebat! </a>
+</div>
+</div>
 
-Andre:<br>
-<p><a href="http://www.trafikanten.no">Trafikanten </a> </p>
-<p><a href="http://www.gulesider.no">Gulesider </a> </p>
-<p><a href="http://www.facebook.com">Facebook </a> </p>
-<p><a href="http://www.youtube.com">Youtube </a> </p>
-<p><a href="http://www.nav.no">NAV </a> </p>
-<p><a href="http://www.seniornett.no/Lenker">Seniornett lenker</a> </p>
-<p><a href="http://www.wikipedia.no">Wikipedia</a> </p>
 
-Antivirus programvare: <br>
-<p><a href="http://www.microsoft.com/security_essentials">Microsoft antivirus</a> </p>
-<p><a href="http://free.avg.com/ww-en/homepage">AVG antivirus</a> </p>
+<strong>informasjonsteknologi:</strong>
+<div id="katogori">
+<div class="kat">
+<strong>Norske:</strong>
+<a href="http://www.dinside.no/data/">DinsideData </a> 
+<a href="http://itpro.no/">ITpro </a>
+<a href="http://www.digi.no">Digi </a> 
+<a href="http://www.itavisen.no">Itavisen </a>
+<a href="http://www.idg.no/computerworld/">Computerworld </a>
+<a href="http://www.idg.no/pcworld/">PCWorld </a>
+<hr/>
+<strong>utenlands:</strong>
+<a href="http://www.bbc.co.uk/news/technology/">bbcTech </a>
+<a href="http://news.cnet.com/latest-news/?tag=hdr;snav">Cnet </a>
+<a href="http://www.computerworld.com/">ComputerWorld </a>
+<a href="http://www.wired.com/">Wired </a>
+</div>
+</div>
+
+<strong>Reise:</strong>
+<div id="katogori">
+<div class="kat">
+<a href="http://www.177.no/"> RuteOpplysningen177</a>
+<a href="http://www.nsb.no">NSB</a>  
+<a href="http://www.trafikanten.no">Trafikanten(Oslo) </a>  
+<hr/>
+<strong>Fly:</strong>
+<a href="http://www.finn.no/reise/">FinnReise </a> 
+<a href="http://www.sas.no/no/?vst=true">SAS </a>
+<a href="http://www.norwegian.com/flight/tips/">Norwegian </a> 
+<a href="http://www.ryanair.com/en">RyanAir </a> 
+
+
+</div>
+</div>
+
+
+
+<strong>Diverse:</strong>
+<div id="katogori">
+<div class="kat">
+<a href="http://www.gulesider.no">Gulesider </a>
+<a href="http://www.facebook.com">Facebook </a> 
+<a href="http://www.youtube.com">Youtube </a> 
+<a href="http://www.nav.no">NAV </a>  
+<a href="http://www.wikipedia.no">Wikipedia</a> 
+<a href="http://www.finn.no/">Finn</a>
+<a href="http://www.google.no/">Google</a>
+<a href="http://no.yahoo.com/">Yahoo</a>
+</div>
+</div>
+
+<strong>Antivirus programvare(Du skal kun ha et antivirus program installert): </strong>
+<div id="katogori">
+<div class="kat"> 
+<a href="http://free.avg.com/ww-en/homepage">AVG antivirus</a> 
+<a href="http://www.avast.com/en-no/index">Avast!</a>
+<a href="http://www.microsoft.com/security_essentials">Microsoft antivirus</a>
+</div>
+</div>
+<p> Vi anbefaler også å ta en tur innom <a href="http://www.seniornett.no/Lenker">Seniornett sine lenker</a> </p>
+
 			  </div>
 			  
 			
